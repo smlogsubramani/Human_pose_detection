df0e7f5424afb5d57ba8eb3952f2ec0dba17fe13
logasubramanism <smlogasubramani@gmail.com>
2023-10-03 22:22:42 +0530

    bicep curl excersie done

--- pose.py
+++ pose.py
@@ -1,79 +1,87 @@
 import cv2
 import mediapipe as mp
-import numpy as np 
+import numpy as np
+# import math
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose  
+bicep_curl_count = 0
+up_phase = True 
+elbow_flexion_threshold = 90 
+elbow_extension_threshold = 150
 
-#https://github.com/google/mediapipe/tree/master/docs/solutions - branch under solution and pose .
 
-#funciton defintion
-
-def calculate_angle(a,b,c):
-    a=np.array(a);
-    b=np.array(b);
-    c=np.array(c);
-    radians = np.arctan(c[1]-b[1] , c[0]-b[0]) - np.arctan(a[1]-b[1] , a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
 
     if(angle > 180.0):
-        angle = 360-angle
-    return angle  
-    # ba = a - b
-    # bc = c - b
+        angle = 360 - angle
+    return angle
 
-    # cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
-
-    # angle_rad = np.arccos(cosine_angle)
-    # angle = np.degrees(angle_rad)
-
-    # return angle
-
-
-
-#creating the vedio feed from the webcam
 cap = cv2.VideoCapture(0)
-# setting up media pipe instance
-with mp_pose.Pose(min_detection_confidence = 0.5,min_tracking_confidence = 0.5) as pose:
 
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
-        ret , frame = cap.read()
+        ret, frame = cap.read()
 
-        #detect image
-
-        #recolor image
-        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        # Recolor image
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
-        #store result 
+        # Store result
         results = pose.process(image)
-        #recolored image back to normal
+        # Recolored image back to normal
         image.flags.writeable = True
-        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         try:
-            landmarks = results.landmarks.landmark
+            landmarks = results.pose_landmarks.landmark
 
-            #get coordinated 
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST].y]
+
+            rshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].y]
+            relbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW].y]
+            rwrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].y]
+
+            # Calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+            rangle = calculate_angle(rshoulder, relbow, rwrist)
+
+            if angle < elbow_flexion_threshold and rangle < elbow_flexion_threshold and up_phase:
+                up_phase = False
+            elif angle > elbow_extension_threshold and rangle > elbow_extension_threshold and not up_phase:
+                up_phase = True  
+                bicep_curl_count += 1  
+
             
-            #calculate angle
-            angle = calculate_angle(shoulder,elbow,wrist)
 
-            #visualize
-            cv2.putText(image,str(angle),tuple(np.multiply(elbow,[640,480]).astype(int)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2,cv2.LINE_AA)
-            print(shoulder ,elbow ,wrist)
+            # Visualize
+            angle_text = f"left elbow Angle: {angle:.2f} degrees" 
+            rangle_text = f"right elbow Angle: {rangle:.2f} degrees"  
+            count_text = f"Bicep Curls: {bicep_curl_count}"
+            cv2.putText(image, count_text, (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            cv2.putText(image, angle_text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            cv2.putText(image, rangle_text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+
+            # Print coordinates
+            # print(f"Shoulder: {shoulder}, Elbow: {elbow}, Wrist: {wrist}")
+            print(f"left-elbow-angle : {angle_text} , right-elbow-angle:{rangle_text}")
         except:
             pass
 
-        #render detection
-        mp_drawing.draw_landmarks(image , results.pose_landmarks,mp_pose.POSE_CONNECTIONS , mp_drawing.DrawingSpec(color=(245,117,66),thickness=2, circle_radius=2),mp_drawing.DrawingSpec(color=(245,66,230),thickness=2, circle_radius=2))
+        # Render detection
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2), mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
 
-        cv2.imshow('Mediapipe Feed' ,image)
+        cv2.imshow('Mediapipe Feed', image)
 
-        if cv2.waitKey(10) & 0xFF == ord('l'): 
+        if cv2.waitKey(10) & 0xFF == ord('l'):
             break
+
 cap.release()
 cv2.destroyAllWindows()
-
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+# import math
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose  
+bicep_curl_count = 0
+up_phase = True 
+elbow_flexion_threshold = 90 
+elbow_extension_threshold = 150
+
 
 def calculate_angle(a, b, c):
     a = np.array(a)
@@ -39,15 +45,34 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].y]
             wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST].y]
 
+            rshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].y]
+            relbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW].y]
+            rwrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].y]
+
             # Calculate angle
             angle = calculate_angle(shoulder, elbow, wrist)
+            rangle = calculate_angle(rshoulder, relbow, rwrist)
+
+            if angle < elbow_flexion_threshold and rangle < elbow_flexion_threshold and up_phase:
+                up_phase = False
+            elif angle > elbow_extension_threshold and rangle > elbow_extension_threshold and not up_phase:
+                up_phase = True  
+                bicep_curl_count += 1  
+
+            
 
             # Visualize
-            angle_text = f"Angle: {angle:.2f} degrees"  # Create the text to display the angle
-            cv2.putText(image, angle_text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            angle_text = f"left elbow Angle: {angle:.2f} degrees" 
+            rangle_text = f"right elbow Angle: {rangle:.2f} degrees"  
+            count_text = f"Bicep Curls: {bicep_curl_count}"
+            cv2.putText(image, count_text, (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            # cv2.putText(image, angle_text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            # cv2.putText(image, rangle_text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
 
             # Print coordinates
-            print(f"Shoulder: {shoulder}, Elbow: {elbow}, Wrist: {wrist}")
+            # print(f"Shoulder: {shoulder}, Elbow: {elbow}, Wrist: {wrist}")
+            # print(f"left-elbow-angle : {angle_text} , right-elbow-angle:{rangle_text}")
+            print(f"bicep curl count :{count_text}")
         except:
             pass
 
